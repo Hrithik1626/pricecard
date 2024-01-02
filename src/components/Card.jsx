41a9c777512a7b75bf6be5d078b48acb30dcafe1
Hrithik1626 <hrithiksaravanan18@gmail.com>
2024-01-02 17:45:31 +0530

    updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,10 +12,10 @@ function Card({data}) {
       <ul className="fa-ul">
         <li className={data.isUser?"":"text-muted"}><span className="fa-li"><i className={data.isUser?"fas fa-check":"fas fa-times"}></i></span>{data.plan!=="FREE"?<b>{data.user}</b>:data.user}</li>
         <li className={data.isStorage?"":"text-muted"}><span className="fa-li"><i className={data.isStorage?"fas fa-check":"fas fa-times"}></i></span>{data.storage}</li>
-        <li className={data.isPublicProjects?"":"text-muted"}><span className="fa-li"><i className={data.isPublicProjects?"fas fa-check":"fas fa-times"}></i></span>{data.publicProjects}</li>
-        <li className={data.isCommunityAccess?"":"text-muted"}><span className="fa-li"><i className={data.isCommunityAccess?"fas fa-check":"fas fa-times"}></i></span>{data.communityAccess}</li>
+        <li className={data.ispublicproducts?"":"text-muted"}><span className="fa-li"><i className={data.isPublicProducts?"fas fa-check":"fas fa-times"}></i></span>{data.PublicProducts}</li>
+        <li className={data.iscommunity?"":"text-muted"}><span className="fa-li"><i className={data.iscommunity?"fas fa-check":"fas fa-times"}></i></span>{data.communityAccess}</li>
         <li className={data.isPrivateProjects?"":"text-muted"}><span className="fa-li"><i className={data.isPrivateProjects?"fas fa-check":"fas fa-times"}></i></span>{data.privateProjects}</li>
-        <li className={data.isPhoneSupport?"":"text-muted"}><span className="fa-li"><i className={data.isPhoneSupport?"fas fa-check":"fas fa-times"}></i></span>{data.phoneSupport}</li>
+        <li className={data.isPhone?"":"text-muted"}><span className="fa-li"><i className={data.isPhone?"fas fa-check":"fas fa-times"}></i></span>{data.phoneSupport}</li>
         <li className={data.isSubDomain?"":"text-muted"}><span className="fa-li"><i className={data.isSubDomain?"fas fa-check":"fas fa-times"}></i></span>{data.plan==="PRO"?<><b>Unlimited</b> {data.subDomain}</>:data.subDomain}</li>
         <li className={data.isReports?"":"text-muted"}><span className="fa-li"><i className={data.isReports?"fas fa-check":"fas fa-times"}></i></span>{data.reports}</li>
       </ul>
